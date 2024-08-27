05b733afc66eb4a23a86ddaed345b6c235191469
Wan Muzaffar <wanmuz86@gmail.com>
2024-08-27 15:57:16 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Search from './components/Search'
+import Weather from './components/Weather'
+
+function App() {
+
+  return (
+    <>
+      <Search/>
+      <Weather/>
+    </>
+  )
+}
+
+export default App
